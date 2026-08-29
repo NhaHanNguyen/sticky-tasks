3a3d0726dff489e759a8050f1cd62e328b511f70
NhaHanNguyen <nhahannguyen14@gmail.com>
2026-08-29 17:35:17 -0400

    firebase google authentication added

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC_ce9B8Gn9u8zBSqsIS8hsCKHcrgpYOgM",
+  authDomain: "sticky-tasks-233fc.firebaseapp.com",
+  projectId: "sticky-tasks-233fc",
+  storageBucket: "sticky-tasks-233fc.firebasestorage.app",
+  messagingSenderId: "1085233021954",
+  appId: "1:1085233021954:web:471508db6e3c4330cc531b"
+};
